0b86eb8655e45c3ec80d6ee27172af20e1705041
Zeke Medley <zekemedley@gmail.com>
2021-06-13 14:09:10 -0500

    Locate live roots on the stack

--- lustc/src/gc.rs
+++ lustc/src/gc.rs
@@ -3,8 +3,7 @@
 //! WARNING: Garbage collection will currently only work on Unix
 //! platforms where the stack grows down.
 
-use cranelift::prelude::Value;
-use cranelift_codegen::binemit::StackMap;
+use cranelift::prelude::*;
 use cranelift_codegen::ir::function::Function;
 use cranelift_codegen::isa::TargetIsa;
 use lazy_static::lazy_static;
@@ -21,7 +20,7 @@ const GC_THRESHOLD: crate::Word = 0; // 100000000;
 
 lazy_static! {
     /// A map between function ids and their stackmaps.
-    static ref SM_REGISTRY: Mutex<Vec<(StackMap, StackMap)>> = Mutex::new(vec![]);
+    static ref SM_REGISTRY: Mutex<Vec<(Vec<usize>, Vec<usize>)>> = Mutex::new(vec![]);
 }
 
 // Shim which will later do garbage collection. Has extremely crude
@@ -33,16 +32,20 @@ pub extern "C" fn do_gc(amount: crate::Word) {
     }
     // Trigger garbage collection if we're using over our gc threshold.
     // memory.
-    if unsafe { false && ALLOC_AMOUNT > GC_THRESHOLD } {
+    if unsafe { true || ALLOC_AMOUNT > GC_THRESHOLD } {
+        println!("GC!");
         backtrace::trace(|frame| {
-            // Resolve this instruction pointer to a symbol name
-            backtrace::resolve_frame(frame, |symbol| {
-                if let Some(name) = symbol.name() {
-                    println!("{}", name);
-                }
-            });
-
             let sp = frame.sp();
+            // When we compile functions we ask them to push
+            // information to their stack about what functon they
+            // are. This allows us to lookup information about what
+            // live references they have on their stack here. The way
+            // this is done is each function pushes
+            // `0xba5eba11<function id>` to their stack when
+            // called. In order to find the function id and perform
+            // the lookup we search for 0xba5eba11 and then the next
+            // word is the id.
+
             // Try to find the totem for 10 iterations. Experiments
             // suggest that Cranelift will put this in the first stack
             // location for the function so we really shouldn't be
@@ -54,7 +57,13 @@ pub extern "C" fn do_gc(amount: crate::Word) {
                 let id: i64 = unsafe { *(sp.offset(-offset) as *const i64) };
                 if id == 0xBA5EBA11 {
                     let id = unsafe { *(sp.offset(-offset + 8) as *const i64) };
-                    println!("id: ({:x})", id);
+                    let registry = SM_REGISTRY.lock().unwrap();
+                    let (escaped, local) = &registry[id as usize];
+                    for offset in local {
+                        let offset = (*offset * 8) as isize;
+                        let val = unsafe { *(sp.offset(offset) as *const i64) };
+                        println!("val: {}", crate::Expr::from_immediate(val));
+                    }
                     break;
                 }
             }
@@ -83,7 +92,6 @@ impl LocalValueCollector {
 
     /// Registers a local value that represents an escaped lust value.
     pub fn register_escaped(&mut self, val: Value) {
-        println!("register escaped");
         self.escaped.push(val)
     }
 
@@ -109,6 +117,47 @@ impl LocalValueCollector {
     }
 }
 
+pub fn root_offsets_from_values(
+    args: &[Value],
+    func: &Function,
+    isa: &dyn TargetIsa,
+) -> Vec<usize> {
+    let loc = &func.locations;
+    let mut live_ref_in_stack_slot = std::collections::HashSet::new();
+    for val in args {
+        if let Some(value_loc) = loc.get(*val) {
+            match *value_loc {
+                codegen::ir::ValueLoc::Stack(stack_slot) => {
+                    live_ref_in_stack_slot.insert(stack_slot);
+                }
+                _ => {}
+            }
+        }
+    }
+
+    let stack = &func.stack_slots;
+    let info = func.stack_slots.layout_info.unwrap();
+
+    let word_size = isa.pointer_bytes() as usize;
+
+    let mut vec = vec![];
+
+    for (ss, ssd) in stack.iter() {
+        if !live_ref_in_stack_slot.contains(&ss)
+            || ssd.kind == codegen::ir::stackslot::StackSlotKind::OutgoingArg
+        {
+            continue;
+        }
+
+        debug_assert!(ssd.size as usize == word_size);
+        let bytes_from_bottom = info.frame_size as i32 + ssd.offset.unwrap();
+        let words_from_bottom = (bytes_from_bottom as usize) / word_size;
+        vec.push(words_from_bottom);
+    }
+
+    vec
+}
+
 /// Registers a pair of stack maps with the stack map registry.
 pub fn register_stackmaps(
     id: i64,
@@ -116,21 +165,15 @@ pub fn register_stackmaps(
     isa: &dyn TargetIsa,
     maps: (Vec<Value>, Vec<Value>),
 ) {
-    let maps = (
-        StackMap::from_values(&maps.0, func, isa),
-        StackMap::from_values(&maps.1, func, isa),
+    let offset_lists = (
+        root_offsets_from_values(&maps.0, func, isa),
+        root_offsets_from_values(&maps.1, func, isa),
     );
     let mut registry = SM_REGISTRY.lock().unwrap();
     // Resize so that we can fit the new number of values. Fill with
     // nonsense.
-    registry.resize(
-        id as usize + 1,
-        (
-            StackMap::from_slice(&[false]),
-            StackMap::from_slice(&[false]),
-        ),
-    );
-    registry[id as usize] = maps;
+    registry.resize(id as usize + 1, (vec![], vec![]));
+    registry[id as usize] = offset_lists;
 }
 
 #[cfg(test)]
